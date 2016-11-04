40ac3e083aead3586c360ff468bc55b69f15335a
Njck <njckfletcher@gmail.com>
2016-11-04 13:23:28 -0400

    progress

--- OOP/src/quadraticclass/Quadratic.java
+++ OOP/src/quadraticclass/Quadratic.java
@@ -1,8 +1,23 @@
 package quadraticclass;
 
 public class Quadratic {
+	String a;
+	String b;
+	String c;
+	
 	public Quadratic(int a, int b, int c) {
+		this.a = String.valueOf(a);
+		this.b = String.valueOf(b);
+		this.c = String.valueOf(c);
+	}
 		
+	public String toString() {
+		if(a == "1") {
+			return "x^2 +" + b + "x + " + c;
+		}
+		else {
+			return a + "x^2 + " + b + "x + " + c;
+		}
 		
 	}
 }
--- OOP/src/quadraticclass/QuadraticRunner.java
+++ OOP/src/quadraticclass/QuadraticRunner.java
@@ -1,6 +1,5 @@
 package quadraticclass;
 
-
 public class QuadraticRunner
 {
   
@@ -12,37 +11,37 @@ public class QuadraticRunner
     Quadratic test3 = new Quadratic(1, 0, 3);
     
     String equation1 = test1.toString();
-    boolean hasSolution1 = test1.hasSolutions();
-    double solution1 = test1.getSolution1();
-    double solution1b = test1.getSolution2();
+    //boolean hasSolution1 = test1.hasSolutions();
+    //double solution1 = test1.getSolution1();
+    //double solution1b = test1.getSolution2();
     
     String equation2 = test2.toString();
-    boolean hasSolution2 = test2.hasSolutions();
-    double solution2 = test2.getSolution1();
-    double solution2b = test2.getSolution2();
+    //boolean hasSolution2 = test2.hasSolutions();
+    //double solution2 = test2.getSolution1();
+    //double solution2b = test2.getSolution2();
     
     String equation3 = test3.toString();
-    boolean hasSolution3 = test3.hasSolutions();
-    double solution3 = test3.getSolution1();
-    double solution3b = test3.getSolution2();
+    //boolean hasSolution3 = test3.hasSolutions();
+    //double solution3 = test3.getSolution1();
+    //double solution3b = test3.getSolution2();
     
     
     System.out.println(equation1);
-    System.out.println(hasSolution1);
-    System.out.println(solution1);
-    System.out.println(solution1b);
-    System.out.println("Expected solutions: -2.0, -3.0");
+    //System.out.println(hasSolution1);
+    //System.out.println(solution1);
+    //System.out.println(solution1b);
+    //System.out.println("Expected solutions: -2.0, -3.0");
     System.out.println();
     System.out.println(equation2);
-    System.out.println(hasSolution2);
-    System.out.println(solution2);
-    System.out.println(solution2b);
-    System.out.println("Expected solution: 2.0");
+    //System.out.println(hasSolution2);
+    //System.out.println(solution2);
+    //System.out.println(solution2b);
+    //System.out.println("Expected solution: 2.0");
     System.out.println();
     System.out.println(equation3);
-    System.out.println(hasSolution3);
-    System.out.println(solution3);
-    System.out.println(solution3b);
-    System.out.println("NaN");
+    //System.out.println(hasSolution3);
+    //System.out.println(solution3);
+    //System.out.println(solution3b);
+    //System.out.println("NaN");
   }
 }
