023a14e1223c5fc7c59c76a94e23a9c593f8d2bf
Njck <njckfletcher@gmail.com>
2016-11-09 13:37:38 -0500

    Major progress on quadratic class

--- OOP/src/quadraticclass/Quadratic.java
+++ OOP/src/quadraticclass/Quadratic.java
@@ -1,23 +1,49 @@
 package quadraticclass;
 
 public class Quadratic {
-	String a;
-	String b;
-	String c;
+	int myA;    // the 'a' in equation
+	int myB;    // the 'b' in equation
+	int myC;    // the 'c' in equation
+	String myEquation;
+	int myDisc; // b*b - 4*a*c
 	
+	// constructor
 	public Quadratic(int a, int b, int c) {
-		this.a = String.valueOf(a);
-		this.b = String.valueOf(b);
-		this.c = String.valueOf(c);
-	}
-		
-	public String toString() {
-		if(a == "1") {
-			return "x^2 +" + b + "x + " + c;
+		myA = a;
+		myB = b;
+		myC = c;
+		findDisc();
+		if(myA == 1) {
+			myEquation = "x^2+" + String.valueOf(myB) + "x+" + String.valueOf(myC);
 		}
 		else {
-			return a + "x^2 + " + b + "x + " + c;
+			myEquation = String.valueOf(myA) + "x^2 + " + String.valueOf(myB) + "x + " + String.valueOf(myC);
 		}
-		
+	}
+	
+	public String toString() {
+		return myEquation;
+	}
+	
+	// calculate first root
+	public double getSolution1() {
+		return (-myB + Math.sqrt(myDisc))/(2*myA);
+	   	}
+	
+	// calculate second root
+	public double getSolution2() {
+		return (-myB - Math.sqrt(myDisc))/(2*myA);
+	}
+	   
+	// check if there are real roots
+	public boolean hasSolutions() {
+		if (myDisc < 0) return false;
+		else return true;
+	}
+	   
+	// calculate discriminant
+	private void findDisc() {
+		myDisc = myB*myB - 4*myA*myC;
 	}
 }
+
