c0c9930c5e5357595b93d22ecc05f161f3ddbb7e
hunter <hunter@ubuntupc>
2016-11-18 19:26:07 -0500

    progress

--- Game/src/game/Parser.java
+++ Game/src/game/Parser.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Parser {
@@ -11,6 +12,21 @@ public class Parser {
 	public String parseCommand() {
 		this.rawCommand = input.nextLine().toLowerCase();
 		parts = rawCommand.split(" ");
+		int removeThe;
+		int removeAt;
+		
+		for (int i = 0; i < parts.length; i++)
+		{
+		    switch(i) {
+		    	case :
+		    }
+		}
+		
+		/*
+		if(Arrays.asList(parts).contains("the")) {
+			
+		}
+		*/
 		
 		return "";
 	}
