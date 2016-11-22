6ace770b451095dfa5cef644609de13cb628aea2
njckfletcher <njckfletcher@gmail.com>
2016-11-21 22:51:42 -0500

    subtle fixes

--- Game/src/game/Main.java
+++ Game/src/game/Main.java
@@ -26,6 +26,7 @@ public class Main {
 							 + "\nname"
 							 + "\ninventory"
 							 + "\nweight"
+							 + "\nlook around"
 							 + "\ngo, goto, move"
 							 + "\ntake, pickup, grab");
 			System.out.println();
--- Game/src/game/Room01.java
+++ Game/src/game/Room01.java
@@ -5,5 +5,4 @@ import java.util.List;
 
 public class Room01 {
 	List<String> inv = new ArrayList<String>();
-	
 }
